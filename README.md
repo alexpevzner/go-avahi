80d4766ec48e146c2567aaf054fc64baf7453e96
Alexander Pevzner <pzz@apevzner.com>
2024-08-05 01:34:54 +0300

    discovery/avahi: added package skeleton

---
+++ README.md
@@ -0,0 +1,10 @@
+# CGo binding for Avahi
+
+```
+import "github.com/alexpevzner/mfp/discovery/avahi"
+```
+
+This package provides CGo binding for Avahi.
+
+<!-- vim:ts=8:sw=4:et:textwidth=72
+-->
---
+++ doc.go
@@ -0,0 +1,11 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Cgo binding for Avahi
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Package documentation
+//
+//go:build linux || freebsd
+
+package avahi
