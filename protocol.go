cf6648791554193c6fd115c99569d1df0d9a84b2
Alexander Pevzner <pzz@apevzner.com>
2024-08-06 10:22:02 +0300

    discovery/avahi: Avahi IP4/IP6 protocol constants

---
+++ protocol.go
@@ -0,0 +1,24 @@
+// MFP - Miulti-Function Printers and scanners toolkit
+// Cgo binding for Avahi
+//
+// Copyright (C) 2024 and up by Alexander Pevzner (pzz@apevzner.com)
+// See LICENSE for license terms and conditions
+//
+// Avahi IP4/IP6 protocol
+//
+//go:build linux || freebsd
+
+package avahi
+
+// #include <avahi-common/address.h>
+import "C"
+
+// Protocol specifies IP4/IP6 protocol
+type Protocol int
+
+// Protocol values:
+const (
+	ProtocolIP4    = C.AVAHI_PROTO_INET
+	ProtocolIP6    = C.AVAHI_PROTO_INET6
+	ProtocolUnspec = C.AVAHI_PROTO_UNSPEC
+)
