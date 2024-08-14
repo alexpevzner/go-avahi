7de84829f68d5bdf1a68d880055242695b054ad2
Alexander Pevzner <pzz@apevzner.com>
2024-08-14 18:57:57 +0300

    Fixed go vet warning in the test

--- avahi_test.go
+++ avahi_test.go
@@ -172,7 +172,8 @@ func addHostNameResolver(cases *[]reflect.SelectCase,
 // TestAvahi performs overall test of most of Avahi API.
 func TestAvahi(t *testing.T) {
 	// Create context with timeout
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
 	// Prepare test data
 	loopback := MustLoopback()
